b4a5555d608dfb584b923472627100f472d33499
Joscha Schmiedt <schmiedt@uni-bremen.de>
2024-05-13 15:32:04 +0200

    Switch to triggered data output

--- src/main.cpp
+++ src/main.cpp
@@ -60,7 +60,7 @@ void IRAM_ATTR readEncoderISR()
 
 constexpr bool circleValues = true;
 constexpr int minValue = 0;
-constexpr int maxValue = 4096;
+constexpr int maxValue = 65536;
 
 void setup()
 {
@@ -78,70 +78,56 @@ void setup()
 int64_t previousPosition = 0;
 int64_t previousStep = 0;
 int64_t previousTimeMS = 0;
-constexpr int minimumOutputIntervalMS = 1;
 
 enum Commands : int8_t
 {
-  kNoCommand = -1,
-  kStartAcquisition = 0,
-  kStopAcquisition = 1,
-  kGetCurrentPosition = 2,
-
+  kNoCommand = 0,
+  kStartAcquisition = 1,
 };
-int incomingByte = 0; // for incoming serial data
 
-struct DataPoint
-{
-  u_int64_t currentTimeMS;
-  int64_t position;
-  int64_t step;
-  int64_t dt;
-} output;
+char inputBuffer[256];
 
 void loop()
 {
 
-  incomingByte = kNoCommand;
-  if (Serial.available() > 0)
+  for (int i = 0; i < 256; ++i)
   {
-    // read the incoming byte:
-    incomingByte = Serial.read();
+    inputBuffer[i] = 0;
   }
 
-  u_int64_t currentTimeMS = 0;
-  // int64_t step = rotaryEncoder.encoderChanged();
-
-  int64_t currentPosition = rotaryEncoder.readEncoder();
+  uint16_t currentPosition = rotaryEncoder.readEncoder();
   uint8_t currentAnalogValue = map(currentPosition, minValue, maxValue, 0, 255);
   dac_output_voltage((dac_channel_t)0, currentAnalogValue);
   dac_output_voltage((dac_channel_t)1, currentAnalogValue);
 
-  currentTimeMS = millis();
-
-  if (incomingByte > 0)
+  while (auto nBytesAvailable = Serial.available() > 0)
   {
-    int64_t step = currentPosition - previousPosition;
-    int64_t dt = currentTimeMS - previousTimeMS;
-
-    // print out the current you read:
-    Serial.print(currentTimeMS);
-    Serial.print(",");
-    Serial.print(previousTimeMS);
-    Serial.print(",");
-    Serial.print(dt);
-    Serial.print(",");
-    Serial.print(currentPosition);
-    Serial.print(",");
-    Serial.print(previousPosition);
-    Serial.print(",");
-    Serial.print(step);
-    Serial.print(",");
-    Serial.print(currentAnalogValue);
-    Serial.print("\n");
-
-    previousPosition = currentPosition;
-    previousTimeMS = currentTimeMS;
+    auto nBytesRead = Serial.readBytes(inputBuffer, nBytesAvailable);
   }
 
-  delay(minimumOutputIntervalMS);
+  // for now, any non-zero input will trigger output
+  if (inputBuffer[0] == 0)
+    return;
+
+  u_int64_t currentTimeMS = millis();
+  int64_t step = currentPosition - previousPosition;
+  int64_t dt = currentTimeMS - previousTimeMS;
+
+  Serial.print(currentTimeMS);
+  Serial.print(",");
+  Serial.print(previousTimeMS);
+  Serial.print(",");
+  Serial.print(dt);
+  Serial.print(",");
+  Serial.print(currentPosition);
+  Serial.print(",");
+  Serial.print(previousPosition);
+  Serial.print(",");
+  Serial.print(step);
+  Serial.print(",");
+  Serial.print(currentAnalogValue);
+  Serial.print("\n");
+
+  previousPosition = currentPosition;
+  previousTimeMS = currentTimeMS;
 }
